4065671461be706ed5a98171538800a5fecc5a34
jotix <jujodeve@gmail.com>
2024-01-14 21:59:46 -0300

    btrfs subvolumenes

--- .scripts/arch-install/base-install.sh
+++ .scripts/arch-install/base-install.sh
@@ -1,6 +1,6 @@
 ### jotix's arch l:inux base install
 ### requisites: 
-### ef00 partition with LABEL=EFI
+### ef00 partition with LABEL=EFI-ARCH
 ### btrfs partition with LABEL=btrfs-vol-1 
 
 DOW="ParallelDownloads = 5"
@@ -9,18 +9,18 @@ sed -i "s/#$DOW/$DOW/g" /etc/pacman.conf
 mount LABEL=btrfs-vol-1 /mnt
 DIR=$PWD
 cd /mnt
-btrfs subvolume create arch
-btrfs subvolume create home
-btrfs subvolume create swap
-btrfs filesystem mkswapfile --size 4g --uuid clear swap/swapfile
+btrfs subvolume create arch/root
+btrfs subvolume create arch/home
+btrfs subvolume create arch/swap
+btrfs filesystem mkswapfile --size 4g --uuid clear arch/swap/swapfile
 cd $DIR
 umount /mnt
 
-mount LABEL=btrfs-vol-1 /mnt -osubvol=arch
+mount LABEL=btrfs-vol-1 /mnt -osubvol=arch/root
 mkdir -p /mnt/home
 mkdir -p /mnt/boot/efi
-mount LABEL=btrfs-vol-1 /mnt/home -osubvol=home
-mount LABEL=EFI /mnt/boot/efi
+mount LABEL=btrfs-vol-1 /mnt/home -osubvol=arch/home
+mount LABEL=EFI-ARCH /mnt/boot/efi
 
 ### extra filesystems
 mkdir -p /mnt/mnt/btrfs-vol-1
@@ -29,7 +29,7 @@ mkdir -p /mnt/mnt/btrfs-vol-3
 mkdir -p /mnt/mnt/Ventoy
 mkdir -p /mnt/mnt/swap
 mount LABEL=btrfs-vol-1 /mnt/mnt/btrfs-vol-1
-mount LABEL=btrfs-vol-1 /mnt/mnt/swap -osubvol=swap
+mount LABEL=btrfs-vol-1 /mnt/mnt/swap -osubvol=arch/swap
 swapon /mnt/mnt/swap/swapfile
 mount LABEL=btrfs-vol-2 /mnt/mnt/btrfs-vol-2
 mount LABEL=btrfs-vol-3 /mnt/mnt/btrfs-vol-3
