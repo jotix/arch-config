0a1fd6ceda859b0ad03cc1b9960833fa2e8cf9c8
jotix <jujodeve@gmail.com>
2024-03-24 14:01:08 -0300

    use only upstream

--- scripts/arch-install/qmk-install.sh
+++ scripts/arch-install/qmk-install.sh
@@ -3,10 +3,12 @@
 sudo pacman -S qmk
 
 cd $HOME
-git clone git@github.com:jotix/qmk_firmware.git
+git clone https://github.com/qmk/qmk_firmware.git
 cd qmk_firmware
-git remote add upstream https://github.com/qmk/qmk_firmware.git
+
 sudo cp util/udev/50-qmk.rules /etc/udev/rules.d/50-qmk.rules
+
 qmk setup
+
 cd $HOME
 
