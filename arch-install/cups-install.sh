e3bbf377dac8ece9b34d2e6987ec81f2e84ab4e9
jotix <jujodeve@gmail.com>
2024-05-14 12:50:12 -0300

    typo

--- arch-install/cups-install.sh
+++ arch-install/cups-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --needed --noconfirm cups ghostscript
-sudo sysyemctl start cups.service
+sudo systemctl start cups.service
 sudo systemctl enable cups.service
 
 sudo pacman -U --noconfirm ./printer-drivers/brother-hl1200-3.0.1-1-x86_64.pkg.tar.zst ./printer-drivers/zijiang-pos58-driver-1.0-2-x86_64.pkg.tar.zst
