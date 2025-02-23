f65b8eebc5bf0ddd3c86e551c29c94a7c145a491
jotix <jujodeve@gmail.com>
2025-02-23 15:09:18 -0300

    Update arch-config-install.sh

--- .scripts/arch-install/arch-config-install.sh
+++ .scripts/arch-install/arch-config-install.sh
@@ -1,14 +1,20 @@
 git clone --bare git@github.com:jotix/arch-config $HOME/.arch-config
+
 function config {
    /usr/bin/git --git-dir=$HOME/.arch-config/ --work-tree=$HOME $@
 }
+
 mkdir -p .config-backup
+
 config checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
+
 config checkout
+
 config config status.showUntrackedFiles no
