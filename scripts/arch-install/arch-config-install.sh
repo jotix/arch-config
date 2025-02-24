931f42700bb055cc02d70ea1fc4787666c06af25
jotix <jujodeve@gmail.com>
2025-02-24 09:58:57 -0300

    dotfiles managed by stow

--- scripts/arch-install/arch-config-install.sh
+++ scripts/arch-install/arch-config-install.sh
@@ -1,20 +1,5 @@
-git clone --bare git@github.com:jotix/arch-config.git $HOME/.arch-config
-                 
-function config {
-   /usr/bin/git --git-dir=$HOME/.arch-config/ --work-tree=$HOME $@
-}
-
-mkdir -p .config-backup
-
-config checkout
-
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-
-config checkout
-
-config config status.showUntrackedFiles no
+git clone git@github.com:jotix/arch-config.git $HOME/arch-config
+cd ~/arch-config/dotfiles
+./unstow.sh
+stow .
+cd
