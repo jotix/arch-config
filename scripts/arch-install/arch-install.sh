8a3f4b4a889e95f946bf3366bafce3c5ec30ea7e
jotix <jujodeve@gmail.com>
2025-03-03 03:53:10 -0300

    bye plasma

--- scripts/arch-install/arch-install.sh
+++ scripts/arch-install/arch-install.sh
@@ -21,12 +21,6 @@ fi
 HOST=$HOST-arch
 
 echo
-DESKTOP=""
-while [[ $DESKTOP != "gnome" ]] && [[ $DESKTOP != "plasma" ]]; do
-    read -p "Which desktop environment (plasma/gnome): " DESKTOP
-done
-
-echo
 read -p "The disk $DISK will be complete deleted. Continue? (yes/no): " CONTINUE
 if [[ $CONTINUE != "yes" ]]; then
     echo "Aborting installation."
@@ -146,17 +140,8 @@ chr locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 
 ### Desktop Environment ########################################################
-case $DESKTOP in
-
-    plasma)
-	chr pacman -S --noconfirm --needed plasma kde-applications kitty system-config-printer tesseract-data-eng
-	chr systemctl enable sddm
-	;;
-
-    gnome)
-	chr pacman -S --noconfirm --needed gnome gnome-extra gnome-browser-connector ghostty
-	chr systemctl enable gdm
-	;;
+chr pacman -S --noconfirm --needed gnome gnome-extra gnome-browser-connector ghostty
+chr systemctl enable gdm
 
 ### install packages ##########################################################
 PACKAGES="
