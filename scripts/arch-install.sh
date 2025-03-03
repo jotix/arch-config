c5d6781edeab17b9bc9c44ede4fb340cf3daaefe
jotix <jujodeve@gmail.com>
2025-03-03 19:21:17 -0300

    firefox install default

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -155,7 +155,7 @@ exa bat fastfetch lsb-release
 ttf-jetbrains-mono ttf-jetbrains-mono-nerd
 ttf-ubuntu-font-family ttf-ubuntu-mono-nerd ttf-ubuntu-nerd
 git lazygit openssh go
-stow emacs chromium rclone
+stow emacs firefox rclone
 helix zed gnu-netcat
 mesa xf86-video-amdgpu vulkan-radeon
 cups ghostscript mpv
